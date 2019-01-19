9c9da155daa8d8260c362fb7e77a72c21c32863d
Verta-master <vertyagina@gmail.com>
2019-01-19 15:09:53 +0600

    Update 2019-01-19T09:09:47.658Z

---
+++ js/range.js
@@ -0,0 +1,15 @@
+// Range slider
+$( function() {
+  $( "#slider-range" ).slider({
+    range: true,
+    min: 0,
+    max: 10000,
+    values: [ 399, 4000 ],
+    slide: function( event, ui ) {
+      $( "#amount-min" ).val( ui.values[ 0 ] );
+      $( "#amount-max" ).val( ui.values[ 1 ] );
+    }
+  });
+  $( "#amount-min" ).val( $( "#slider-range" ).slider( "values", 0 ) );
+  $( "#amount-max" ).val( $( "#slider-range" ).slider( "values", 1 ) );
+});
